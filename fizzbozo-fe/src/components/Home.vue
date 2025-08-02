d6d32529a3ab6d3832ac6897dac8e5383e37c057
Hoang Bao Phuc Chau <phcchu88@gmail.com>
2025-08-02 21:22:06 +1000

    add homepage component

---
+++ fizzbozo-fe/src/components/Home.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+    <div class = 'container home-container'>
+        <h1 class="text-center mt-5">FizzBozo</h1>
+        <p class="text-center home-text">New way to enjoy FizzBuzz game</p>
+        <p class="text-center home-text">Create, Mange or Play by the way you like</p>
+        <p class="text-center home-text">Unlimited Experience</p>
+        <div class = "row mt-4">
+            <div class="d-flex justify-content-center align-items-center flex-column">
+                <button class="btn btn-bd-primary btn-lg mb-3">Play Now</button>
+            </div>
+        </div>
+        <p class="text-center home-text mt-4">Got other things in mind? Checkout these options</p>
+            <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 text-center"> 
+                <button class="btn btn-bd-primary-1 btn-lg mb-3">View <br> History</button>
+            </div>
+            <div class="col-md-4 text-center">
+                <button class="btn btn-bd-primary-2 btn-lg mb-3">Create <br> Games</button>
+            </div>
+            <div class="col-md-4 text-center">
+                <button class="btn btn-bd-primary-3 btn-lg mb-3">Leaderboard</button>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+</style>
---
+++ fizzbozo-fe/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary pt-3 pb-3 ps-lg-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">FizzBozo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0 ">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">History</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Leaderboard</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+</style>
