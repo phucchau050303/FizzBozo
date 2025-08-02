c3a7fdab33e11db71c927b917211aa2d06ff18e6
Hoang Bao Phuc Chau <phcchu88@gmail.com>
2025-08-03 02:00:26 +1000

    add Footer component

--- fizzbozo-fe/src/App.vue
+++ fizzbozo-fe/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
-import Home from './components/Home.vue'
+import HomeView from './views/HomeView.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Navbar />
-  <Home />
+  <HomeView />
+  <Footer />
 </template>
 
 <style scoped>
