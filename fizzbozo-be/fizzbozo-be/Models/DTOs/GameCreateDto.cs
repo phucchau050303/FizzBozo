c404144d09e0dbaaad1863829904c6953ae9a28f
Hoang Bao Phuc Chau <phcchu88@gmail.com>
2025-08-10 01:37:57 +1000

    add DTOs

---
+++ fizzbozo-be/fizzbozo-be/Models/DTOs/GameCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace fizzbozo_be.Models.DTOs
+{
+    public class GameCreateDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Range(0,1000)]
+        public int MinNumber { get; set; }
+
+        [Range(0,1000)]
+        public int MaxNumber { get; set; }
+
+        public List<RuleCreateDto> Rules { get; set; } = new();
+
+    }
+}
---
+++ fizzbozo-be/fizzbozo-be/Models/DTOs/RuleCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fizzbozo_be.Models.DTOs
+{
+    public class RuleCreateDto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Divisor must be greater than 0.")]
+        public int DividedBy { get; set; }
+
+        [Required(ErrorMessage = "Divisor must be greater than 0.")]
+        public string Word { get; set; }
+    }
+}
